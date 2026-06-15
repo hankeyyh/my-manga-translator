7e465027788dae56e038ef70a462943f4172e6ff
yuhanyang <yuhanyang@futunn.com>
2026-06-15 10:58:55 +0800

    feat: update wrangler vars

--- biz/utils/url.ts
+++ biz/utils/url.ts
@@ -2,6 +2,3 @@ export function getAlgoBaseUrl() {
     return process.env.MANGA_IMAGE_TRANSLATOR_BASE_URL || 'http://127.0.0.1:8000';
 }
 
-export function getBaseUrl() {
-    return process.env.NEXT_PUBLIC_SITE_URL!;
-}
