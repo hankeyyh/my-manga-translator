02436c05f52f6fb9c892d0a7c4649cc0cf4ccf91
yuhanyang <yuhanyang@futunn.com>
2026-09-19 17:49:51 +0800

    fix: stripe js 懒下载

--- biz/utils/stripe/client.ts
+++ biz/utils/stripe/client.ts
@@ -1,4 +1,10 @@
-import { loadStripe } from "@stripe/stripe-js";
+/**
+ * @stripe/stripe-js/pure：import 时不会自动下载stripe的CDN脚本(240KB)，只有调用loadStripe时才懒下载
+ * @stripe/stripe-js：默认import时就下载
+ * 
+ * 本项目只在更改订阅计划，且需要3DS验证时才使用
+ */
+import { loadStripe } from "@stripe/stripe-js/pure";
 
 let stripePromise: ReturnType<typeof loadStripe> | null = null;
 
